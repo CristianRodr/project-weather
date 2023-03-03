402eb55738012c7e1de54f6129fa93effa1242ee
Jorgemomo <jmomo581@gmail.com>
2023-03-02 22:44:35 -0500

    Revert "Maquetación segunda sección"
    This reverts commit c645a2611e21be6f6481741fed096743a87e6e63.

--- js/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const link = document.querySelector(".verMas");
-
-const openHidden = function () {
-  document.querySelector(".details__conditions").classList.remove("hidden");
-};
-
-link.addEventListener("click", openHidden);
