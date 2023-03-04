888938a655f38a9aeec92c28f71102532c08769a
cristiangarzon22 <86244196+cristiangarzon22@users.noreply.github.com>
2023-03-03 20:40:56 -0500

    new commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,33 @@
-console.log("hola mundo");
+import Chart from 'chart.js/auto'
+
+function grafica() {
+  const data = [
+    { year: 2010, count: 10 },
+    { year: 2011, count: 20 },
+    { year: 2012, count: 15 },
+    { year: 2013, count: 25 },
+    { year: 2014, count: 22 },
+    { year: 2015, count: 30 },
+    { year: 2016, count: 28 },
+  ];
+
+  new Chart(
+    document.getElementById('acquisitions'),
+    {
+      type: 'line',
+      data: {
+        labels: data.map(row => row.year),
+        datasets: [
+          {
+            label: 'Acquisitions by year',
+            data: data.map(row => row.count),
+            borderColor: '#ff8000',
+          }
+        ]
+      }
+    }
+  );
+}
+
+grafica();
+
